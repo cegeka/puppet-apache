puppetlabs/puppetlabs-apache

commit f41251e336fa3e7c31c19968ccee74121cf71e47

Narrow down Datatypes
- use Datatypes from Stdlib, particularly a Port now only accepts Stdlib::Port
- use Variant[Integer, Pattern[/^[0-9]+$/]] where Strings represent Integers

manifests/vhost/custom.pp
@@ -17,9 +17,9 @@
 #
 define apache::vhost::custom (
   String $content,
-  String $ensure                            = 'present',
-  Variant[Integer,String,Boolean] $priority = '25',
-  Boolean $verify_config                    = true,
+  String $ensure                      = 'present',
+  Variant[Integer, Boolean] $priority = 25,
+  Boolean $verify_config              = true,
 ) {
   include apache
 

manifests/vhost/fragment.pp
@@ -29,11 +29,11 @@
 # @example With a vhost with priority
 #   include apache
 #   apache::vhost { 'myvhost':
-#     priority => '42',
+#     priority => 42,
 #   }
 #   apache::vhost::fragment { 'myfragment':
 #     vhost    => 'myvhost',
-#     priority => '42',
+#     priority => 42,
 #     content  => '# Foo',
 #   }
 #
@@ -44,7 +44,7 @@
 #   }
 #   apache::vhost::fragment { 'myfragment':
 #     vhost    => 'myvhost',
-#     priority => '10', # default_vhost implies priority 10
+#     priority => 10, # default_vhost implies priority 10
 #     content  => '# Foo',
 #   }
 #
@@ -52,16 +52,16 @@
 #   include apache
 #   apache::vhost::fragment { 'myfragment':
 #     vhost    => 'default',
-#     priority => '15',
+#     priority => 15,
 #     content  => '# Foo',
 #   }
 #
 define apache::vhost::fragment (
   String[1] $vhost,
-  Optional[Integer[0]] $port                          = undef,
-  Optional[Variant[Integer,String,Boolean]] $priority = undef,
-  Optional[String] $content                           = undef,
-  Integer[0] $order                                   = 900,
+  Optional[Stdlib::Port] $port                  = undef,
+  Optional[Variant[Integer, Boolean]] $priority = undef,
+  Optional[String] $content                     = undef,
+  Integer[0] $order                             = 900,
 ) {
   # This copies the logic from apache::vhost
   if $priority {

manifests/vhost/proxy.pp
@@ -92,11 +92,11 @@
 #       { 'path' => '/l', 'url' => 'http://backend-xy',
 #         'reverse_urls' => ['http://backend-x', 'http://backend-y'] },
 #       { 'path' => '/d', 'url' => 'http://backend-a/d',
-#         'params' => { 'retry' => '0', 'timeout' => '5' }, },
+#         'params' => { 'retry' => 0, 'timeout' => 5 }, },
 #       { 'path' => '/e', 'url' => 'http://backend-a/e',
 #         'keywords' => ['nocanon', 'interpolate'] },
 #       { 'path' => '/f', 'url' => 'http://backend-f/',
-#         'setenv' => ['proxy-nokeepalive 1','force-proxy-request-1.0 1']},
+#         'setenv' => ['proxy-nokeepalive 1', 'force-proxy-request-1.0 1']},
 #       { 'path' => '/g', 'url' => 'http://backend-g/',
 #         'reverse_cookies' => [{'path' => '/g', 'url' => 'http://backend-g/',}, {'domain' => 'http://backend-g', 'url' => 'http:://backend-g',},], },
 #       { 'path' => '/h', 'url' => 'http://backend-h/h',
@@ -106,20 +106,20 @@
 #
 define apache::vhost::proxy (
   String[1] $vhost,
-  Optional[Variant[Integer,String,Boolean]] $priority = undef,
-  Integer[0] $order = 170,
-  Optional[Stdlib::Port] $port = undef,
-  Optional[String[1]] $proxy_dest = undef,
-  Optional[String[1]] $proxy_dest_match = undef,
-  Optional[String[1]] $proxy_dest_reverse_match = undef,
-  Variant[Array[String[1]], String[1]] $no_proxy_uris = [],
-  Variant[Array[String[1]], String[1]] $no_proxy_uris_match = [],
-  Optional[Array[Apache::Vhost::ProxyPass]] $proxy_pass = undef,
+  Optional[Variant[Integer, Boolean]] $priority               = undef,
+  Integer[0] $order                                           = 170,
+  Optional[Stdlib::Port] $port                                = undef,
+  Optional[String[1]] $proxy_dest                             = undef,
+  Optional[String[1]] $proxy_dest_match                       = undef,
+  Optional[String[1]] $proxy_dest_reverse_match               = undef,
+  Variant[Array[String[1]], String[1]] $no_proxy_uris         = [],
+  Variant[Array[String[1]], String[1]] $no_proxy_uris_match   = [],
+  Optional[Array[Apache::Vhost::ProxyPass]] $proxy_pass       = undef,
   Optional[Array[Apache::Vhost::ProxyPass]] $proxy_pass_match = undef,
-  Boolean $proxy_requests = false,
-  Boolean $proxy_preserve_host = false,
-  Optional[Boolean] $proxy_add_headers = undef,
-  Boolean $proxy_error_override = false,
+  Boolean $proxy_requests                                     = false,
+  Boolean $proxy_preserve_host                                = false,
+  Optional[Boolean] $proxy_add_headers                        = undef,
+  Boolean $proxy_error_override                               = false,
 ) {
   include apache::mod::proxy
   include apache::mod::proxy_http

examples/vhost.pp
@@ -9,13 +9,13 @@ class { 'apache': }
 
 # Most basic vhost
 apache::vhost { 'first.example.com':
-  port    => '80',
+  port    => 80,
   docroot => '/var/www/first',
 }
 
 # Vhost with different docroot owner/group/mode
 apache::vhost { 'second.example.com':
-  port          => '80',
+  port          => 80,
   docroot       => '/var/www/second',
   docroot_owner => 'third',
   docroot_group => 'third',
@@ -24,21 +24,21 @@ apache::vhost { 'second.example.com':
 
 # Vhost with serveradmin
 apache::vhost { 'third.example.com':
-  port        => '80',
+  port        => 80,
   docroot     => '/var/www/third',
   serveradmin => 'admin@example.com',
 }
 
 # Vhost with ssl (uses default ssl certs)
 apache::vhost { 'ssl.example.com':
-  port    => '443',
+  port    => 443,
   docroot => '/var/www/ssl',
   ssl     => true,
 }
 
 # Vhost with ssl and specific ssl certs
 apache::vhost { 'fourth.example.com':
-  port     => '443',
+  port     => 443,
   docroot  => '/var/www/fourth',
   ssl      => true,
   ssl_cert => '/etc/ssl/fourth.example.com.cert',
@@ -48,7 +48,7 @@ apache::vhost { 'fourth.example.com':
 # Vhost with english title and servername parameter
 apache::vhost { 'The fifth vhost':
   servername => 'fifth.example.com',
-  port       => '80',
+  port       => 80,
   docroot    => '/var/www/fifth',
 }
 
@@ -58,13 +58,13 @@ apache::vhost { 'sixth.example.com':
     'sixth.example.org',
     'sixth.example.net',
   ],
-  port          => '80',
+  port          => 80,
   docroot       => '/var/www/fifth',
 }
 
 # Vhost with alternate options
 apache::vhost { 'seventh.example.com':
-  port    => '80',
+  port    => 80,
   docroot => '/var/www/seventh',
   options => [
     'Indexes',
@@ -74,14 +74,14 @@ apache::vhost { 'seventh.example.com':
 
 # Vhost with AllowOverride for .htaccess
 apache::vhost { 'eighth.example.com':
-  port     => '80',
+  port     => 80,
   docroot  => '/var/www/eighth',
   override => 'All',
 }
 
 # Vhost with access and error logs disabled
 apache::vhost { 'ninth.example.com':
-  port       => '80',
+  port       => 80,
   docroot    => '/var/www/ninth',
   access_log => false,
   error_log  => false,
@@ -89,7 +89,7 @@ apache::vhost { 'ninth.example.com':
 
 # Vhost with custom access and error logs and logroot
 apache::vhost { 'tenth.example.com':
-  port            => '80',
+  port            => 80,
   docroot         => '/var/www/tenth',
   access_log_file => 'tenth_vhost.log',
   error_log_file  => 'tenth_vhost_error.log',
@@ -98,14 +98,14 @@ apache::vhost { 'tenth.example.com':
 
 # Vhost with a cgi-bin
 apache::vhost { 'eleventh.example.com':
-  port        => '80',
+  port        => 80,
   docroot     => '/var/www/eleventh',
   scriptalias => '/usr/lib/cgi-bin',
 }
 
 # Vhost with a proxypass configuration
 apache::vhost { 'twelfth.example.com':
-  port          => '80',
+  port          => 80,
   docroot       => '/var/www/twelfth',
   proxy_dest    => 'http://internal.example.com:8080/twelfth',
   no_proxy_uris => ['/login','/logout'],
@@ -113,7 +113,7 @@ apache::vhost { 'twelfth.example.com':
 
 # Vhost to redirect /login and /logout
 apache::vhost { 'thirteenth.example.com':
-  port            => '80',
+  port            => 80,
   docroot         => '/var/www/thirteenth',
   redirect_source => [
     '/login',
@@ -127,7 +127,7 @@ apache::vhost { 'thirteenth.example.com':
 
 # Vhost to permamently redirect
 apache::vhost { 'fourteenth.example.com':
-  port            => '80',
+  port            => 80,
   docroot         => '/var/www/fourteenth',
   redirect_source => '/blog',
   redirect_dest   => 'http://blog.example.com',
@@ -136,7 +136,7 @@ apache::vhost { 'fourteenth.example.com':
 
 # Vhost with a rack configuration
 apache::vhost { 'fifteenth.example.com':
-  port           => '80',
+  port           => 80,
   docroot        => '/var/www/fifteenth',
   rack_base_uris => ['/rackapp1', '/rackapp2'],
 }
@@ -144,7 +144,7 @@ apache::vhost { 'fifteenth.example.com':
 # Vhost to redirect non-ssl to ssl
 apache::vhost { 'sixteenth.example.com non-ssl':
   servername => 'sixteenth.example.com',
-  port       => '80',
+  port       => 80,
   docroot    => '/var/www/sixteenth',
   rewrites   => [
     {
@@ -158,7 +158,7 @@ apache::vhost { 'sixteenth.example.com non-ssl':
 # Rewrite a URL to lower case
 apache::vhost { 'sixteenth.example.com non-ssl':
   servername => 'sixteenth.example.com',
-  port       => '80',
+  port       => 80,
   docroot    => '/var/www/sixteenth',
   rewrites   => [
     { comment      => 'Rewrite to lower case',
@@ -171,7 +171,7 @@ apache::vhost { 'sixteenth.example.com non-ssl':
 
 apache::vhost { 'sixteenth.example.com ssl':
   servername => 'sixteenth.example.com',
-  port       => '443',
+  port       => 443,
   docroot    => '/var/www/sixteenth',
   ssl        => true,
 }
@@ -179,41 +179,41 @@ apache::vhost { 'sixteenth.example.com ssl':
 # Vhost to redirect non-ssl to ssl using old rewrite method
 apache::vhost { 'sixteenth.example.com non-ssl old rewrite':
   servername   => 'sixteenth.example.com',
-  port         => '80',
+  port         => 80,
   docroot      => '/var/www/sixteenth',
   rewrite_cond => '%{HTTPS} off',
   rewrite_rule => '(.*) https://%{HTTP_HOST}%{REQUEST_URI}',
 }
 apache::vhost { 'sixteenth.example.com ssl old rewrite':
   servername => 'sixteenth.example.com',
-  port       => '443',
+  port       => 443,
   docroot    => '/var/www/sixteenth',
   ssl        => true,
 }
 
 # Vhost to block repository files
 apache::vhost { 'seventeenth.example.com':
-  port    => '80',
+  port    => 80,
   docroot => '/var/www/seventeenth',
   block   => 'scm',
 }
 
 # Vhost with special environment variables
 apache::vhost { 'eighteenth.example.com':
-  port    => '80',
+  port    => 80,
   docroot => '/var/www/eighteenth',
   setenv  => ['SPECIAL_PATH /foo/bin','KILROY was_here'],
 }
 
 apache::vhost { 'nineteenth.example.com':
-  port     => '80',
+  port     => 80,
   docroot  => '/var/www/nineteenth',
   setenvif => 'Host "^([^\.]*)\.website\.com$" CLIENT_NAME=$1',
 }
 
 # Vhost with additional include files
 apache::vhost { 'twentyieth.example.com':
-  port                => '80',
+  port                => 80,
   docroot             => '/var/www/twelfth',
   additional_includes => ['/tmp/proxy_group_a','/tmp/proxy_group_b'],
 }
@@ -222,7 +222,7 @@ apache::vhost { 'twentyieth.example.com':
 # http://example.com.loc => /var/www/example.com
 apache::vhost { 'subdomain.loc':
   vhost_name      => '*',
-  port            => '80',
+  port            => 80,
   virtual_docroot => '/var/www/%-2+',
   docroot         => '/var/www',
   serveraliases   => ['*.loc',],
@@ -230,9 +230,9 @@ apache::vhost { 'subdomain.loc':
 
 # Vhost with SSL (SSLProtocol, SSLCipherSuite & SSLHonorCipherOrder from default)
 apache::vhost { 'securedomain.com':
-  priority   => '10',
+  priority   => 10,
   vhost_name => 'www.securedomain.com',
-  port       => '443',
+  port       => 443,
   docroot    => '/var/www/secure',
   ssl        => true,
   ssl_cert   => '/etc/ssl/securedomain.cert',
@@ -243,14 +243,14 @@ apache::vhost { 'securedomain.com':
 
 # Vhost with access log environment variables writing control
 apache::vhost { 'twentyfirst.example.com':
-  port               => '80',
+  port               => 80,
   docroot            => '/var/www/twentyfirst',
   access_log_env_var => 'admin',
 }
 
 # Vhost with a passenger_base configuration
 apache::vhost { 'twentysecond.example.com':
-  port           => '80',
+  port           => 80,
   docroot        => '/var/www/twentysecond',
   rack_base_uris => ['/passengerapp1', '/passengerapp2'],
 }

cegeka/puppet-apache

commit 02c56563710ded692d4f5d7355365ce463d08867

Allow vhosts to have a string priority again
In f41251e336fa3e7c31c19968ccee74121cf71e47 the type was narrowed to no
longer allow strings, but this can cause problems.

Sorting is alphabetical and you need to format it for the correct
sorting. So to make sure 2 loads before 10 you need to format it as 02.
While the vhost can do some printf style magic to change 2 to 02, it
must then also know what the highest number is. Otherwise 100 is sorted
before 20. By allowing strings, you allow the caller to fix this.

A type alias is introduced to reduce duplication and make it easier to
track.

Fixes: f41251e336fa3e7c31c19968ccee74121cf71e47

manifests/vhost.pp
@@ -1730,7 +1730,7 @@ define apache::vhost (
   Optional[Integer] $ssl_stapling_timeout                                             = undef,
   Optional[Enum['on', 'off']] $ssl_stapling_return_errors                             = undef,
   Optional[String] $ssl_user_name                                                     = undef,
-  Optional[Variant[Integer, Boolean]] $priority                                       = undef,
+  Optional[Apache::Vhost::Priority] $priority                                         = undef,
   Boolean $default_vhost                                                              = false,
   Optional[String] $servername                                                        = $name,
   Variant[Array[String], String] $serveraliases                                       = [],

manifests/vhost/custom.pp
@@ -18,7 +18,7 @@
 define apache::vhost::custom (
   String $content,
   String $ensure                      = 'present',
-  Variant[Integer, Boolean] $priority = 25,
+  Apache::Vhost::Priority $priority   = 25,
   Boolean $verify_config              = true,
 ) {
   include apache

manifests/vhost/fragment.pp
@@ -59,7 +59,7 @@
 define apache::vhost::fragment (
   String[1] $vhost,
   Optional[Stdlib::Port] $port                  = undef,
-  Optional[Variant[Integer, Boolean]] $priority = undef,
+  Optional[Apache::Vhost::Priority] $priority   = undef,
   Optional[String] $content                     = undef,
   Integer[0] $order                             = 900,
 ) {

manifests/vhost/proxy.pp
@@ -106,7 +106,7 @@
 #
 define apache::vhost::proxy (
   String[1] $vhost,
-  Optional[Variant[Integer, Boolean]] $priority               = undef,
+  Optional[Apache::Vhost::Priority] $priority                 = undef,
   Integer[0] $order                                           = 170,
   Optional[Stdlib::Port] $port                                = undef,
   Optional[String[1]] $proxy_dest                             = undef,

types/vhost/priority.pp
@@ -1,0 +1,2 @@
+# @summary The priority on vhost
+type Apache::Vhost::Priority = Variant[Pattern[/^\d+$/], Integer, Boolean]

spec/type_aliases/vhost_priority_spec.rb
@@ -1,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Apache::Vhost::Priority' do
+  # Pattern
+  it { is_expected.to allow_value('10') }
+  it { is_expected.to allow_value('010') }
+  it { is_expected.not_to allow_value('') }
+  it { is_expected.not_to allow_value('a') }
+  it { is_expected.not_to allow_value('a1') }
+  it { is_expected.not_to allow_value('1a') }
+
+  # Integer
+  it { is_expected.to allow_value(0) }
+  it { is_expected.to allow_value(1) }
+
+  # Boolean
+  it { is_expected.to allow_value(true) } # Technically an illegal value
+  it { is_expected.to allow_value(false) }
+
+  it { is_expected.not_to allow_value(nil) }
+end