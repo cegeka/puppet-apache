puppetlabs/puppetlabs-apache

commit fc6cced03e60c5a78bceb006dd4267fc37f54959

Committing for holding
Todo:
- doc headers for relevant manifests
- Update readme
- Commit separately

manifests/init.pp
@@ -13,20 +13,36 @@
 # Sample Usage:
 #
 class apache (
-  $default_mods = true,
-  $service_enable = true,
-  $serveradmin  = 'root@localhost',
-  $sendfile     = false
-) {
-  include apache::params
+  $default_mods         = true,
+  $default_vhost        = true,
+  $default_ssl_vhost    = true,
+  $default_ssl_cert     = $apache::params::default_ssl_cert,
+  $default_ssl_key      = $apache::params::default_ssl_key,
+  $default_ssl_chain    = undef,
+  $default_ssl_ca       = undef,
+  $default_ssl_crl_path = undef,
+  $default_ssl_crl      = undef,
+  $service_enable       = true,
+  $serveradmin          = 'root@localhost',
+  $sendfile             = false,
+  $error_pages          = false
+) inherits apache::params {
 
   package { 'httpd':
     ensure => installed,
     name   => $apache::params::apache_name,
   }
 
+  validate_bool($default_mods)
+  validate_bool($default_vhost)
   # true/false is sufficient for both ensure and enable
   validate_bool($service_enable)
+
+  $user       = $apache::params::user
+  $group      = $apache::params::group
+  $httpd_dir  = $apache::params::httpd_dir
+  $ports_file = $apache::params::ports_file
+  $logroot    = $apache::params::logroot
 
   service { 'httpd':
     ensure    => $service_enable,
@@ -35,38 +51,101 @@ class apache (
     subscribe => Package['httpd'],
   }
 
-  file { 'httpd_vdir':
+  file { $apache::params::confd_dir:
     ensure  => directory,
-    path    => $apache::params::vdir,
     recurse => true,
     purge   => true,
     notify  => Service['httpd'],
     require => Package['httpd'],
   }
 
+  file { $apache::params::mod_dir:
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+    notify  => Service['httpd'],
+    require => Package['httpd'],
+  }
+
+  file { $apache::params::vhost_dir:
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+    notify  => Service['httpd'],
+    require => Package['httpd'],
+  }
+
+  concat { $ports_file:
+    owner  => $user,
+    group  => $group,
+    mode   => '0644',
+    notify => Service[$apache::params::apache_name],
+  }
+  concat::fragment { "Apache ports header":
+    target  => $ports_file,
+    content => template('apache/ports_header.erb')
+  }
+
   if $apache::params::conf_dir and $apache::params::conf_file {
+    case $::osfamily {
+      'debian': {
+        $docroot          = '/var/www'
+        $pidfile          = '${APACHE_PID_FILE}'
+        $error_log        = 'error.log'
+        $error_pages_path = '/usr/share/apache2/error'
+        $scriptalias      = '/usr/lib/cgi-bin'
+        $access_log_file  = 'access.log'
+      }
+      'redhat': {
+        $docroot          = '/var/www/html'
+        $pidfile          = 'run/httpd.pid'
+        $error_log        = 'error_log'
+        $error_pages_path = '/var/www/error'
+        $scriptalias      = '/var/www/cgi-bin'
+        $access_log_file  = 'access_log'
+      }
+    }
     # Template uses:
-    # - $apache::params::user
-    # - $apache::params::group
-    # - $apache::params::conf_dir
-    # - $serveradmin
+    # - $httpd_dir
+    # - $pidfile
+    # - $user
+    # - $group
+    # - $logroot
+    # - $error_log
+    # - $sendfile
+    # - $mod_dir
+    # - $ports_file
+    # - $confd_dir
+    # - $vhost_dir
+    # - $error_pages
+    # - $error_pages_path
     file { "${apache::params::conf_dir}/${apache::params::conf_file}":
       ensure  => present,
-      content => template("apache/${apache::params::conf_file}.erb"),
+      content => template("apache/httpd.conf.erb"),
       notify  => Service['httpd'],
       require => Package['httpd'],
     }
-    if $default_mods == true {
+    if $default_mods {
       include apache::mod::default
     }
-  }
-  if $apache::params::mod_dir {
-    file { $apache::params::mod_dir:
-      ensure  => directory,
-      require => Package['httpd'],
-    } -> A2mod <| |>
-    resources { 'a2mod':
-      purge => true,
+    if $default_vhost == true {
+      apache::vhost { 'default':
+        port            => 80,
+        docroot         => $docroot,
+        scriptalias     => $scriptalias,
+        serveradmin     => $serveradmin,
+        access_log_file => $access_log_file,
+      }
+    }
+    if $default_ssl_vhost {
+      apache::vhost { 'default-ssl':
+        port            => 443,
+        ssl             => true,
+        docroot         => $docroot,
+        scriptalias     => $scriptalias,
+        serveradmin     => $serveradmin,
+        access_log_file => "ssl_${access_log_file}",
+      }
     }
   }
 }

spec/classes/apache_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe 'apache', :type => :class do
-  context "On a Debian OS" do
+  context "on a Debian OS" do
     let :facts do
-      { :osfamily => 'Debian' }
+      {
+        :osfamily       => 'Debian',
+        :concat_basedir => '/dne',
+      }
     end
     it { should include_class("apache::params") }
     it { should contain_package("httpd") }
@@ -13,7 +16,7 @@ describe 'apache', :type => :class do
       'subscribe' => 'Package[httpd]'
       )
     }
-    it { should contain_file("httpd_vdir").with(
+    it { should contain_file("/etc/apache2/sites-enabled").with(
       'ensure'  => 'directory',
       'recurse' => 'true',
       'purge'   => 'true',
@@ -22,9 +25,12 @@ describe 'apache', :type => :class do
       )
     }
   end
-  context "On a RedHat OS" do
+  context "on a RedHat OS" do
     let :facts do
-      { :osfamily => 'RedHat' }
+      {
+        :osfamily       => 'RedHat',
+        :concat_basedir => '/dne',
+      }
     end
     it { should include_class("apache::params") }
     it { should contain_package("httpd") }
@@ -34,7 +40,7 @@ describe 'apache', :type => :class do
       'subscribe' => 'Package[httpd]'
       )
     }
-    it { should contain_file("httpd_vdir").with(
+    it { should contain_file("/etc/httpd/site.d").with(
       'ensure'  => 'directory',
       'recurse' => 'true',
       'purge'   => 'true',

cegeka/puppet-apache

commit 3450671801ad7d2cb61a2662c2c86e251b2bef9a

Make serveradmin an optional parameter and use it
Since fc6cced03e60c5a78bceb006dd4267fc37f54959 the parameter no longer
ends up in the main configuration. It was still used in
apache::mod::shib until 049fd54b57e8a4f28ceeea4888456eafe1509fa1. Since
then it's been unused.

This changes it to an optional non-empty string which means it doesn't
change the default config, but still allows users to set the ServerAdmin
in the main Apache config.

Fixes: fc6cced03e60c5a78bceb006dd4267fc37f54959

manifests/init.pp
@@ -489,7 +489,7 @@ class apache (
   Boolean $purge_configs                                                     = true,
   Optional[Boolean] $purge_vhost_dir                                         = undef,
   Boolean $purge_vdir                                                        = false,
-  String $serveradmin                                                        = 'root@localhost',
+  Optional[String[1]] $serveradmin                                           = undef,
   Enum['On', 'Off', 'on', 'off'] $sendfile                                   = 'On',
   Optional[Enum['On', 'Off', 'on', 'off']] $ldap_verify_server_cert          = undef,
   Optional[String] $ldap_trusted_mode                                        = undef,

templates/httpd.conf.erb
@@ -5,6 +5,9 @@ TraceEnable <%= scope.call_function('apache::bool2httpd', [@trace_enable]) %>
 
 ServerName "<%= @servername %>"
 ServerRoot "<%= @server_root %>"
+<%- if @serveradmin -%>
+ServerAdmin <%= @serveradmin %>
+<%- end -%>
 PidFile <%= @pidfile %>
 Timeout <%= @timeout %>
 KeepAlive <%= @keepalive %>

spec/classes/apache_spec.rb
@@ -39,6 +39,7 @@ describe 'apache', type: :class do
         'purge' => 'false'
       ).that_notifies('Class[Apache::Service]').that_requires('Package[httpd]')
     }
+    it { is_expected.to contain_file('/etc/apache2/apache2.conf').without_content(%r{ServerAdmin}) }
     it {
       is_expected.to contain_concat('/etc/apache2/ports.conf').with(
         'owner' => 'root', 'group' => 'root',
@@ -81,6 +82,16 @@ describe 'apache', type: :class do
       end
 
       it { is_expected.to contain_file('/etc/apache2/apache2.conf').with_content %r{^IncludeOptional "/etc/apache2/conf\.d/\*\.conf"$} }
+    end
+
+    context 'with serveradmin' do
+      let(:params) do
+        {
+          serveradmin: 'admin@example.com',
+        }
+      end
+
+      it { is_expected.to contain_file('/etc/apache2/apache2.conf').with_content %r{^ServerAdmin admin@example\.com$} }
     end
 
     context 'when specifying slash encoding behaviour' do